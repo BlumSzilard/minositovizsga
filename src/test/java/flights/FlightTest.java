0e9ca2701b394cdcae66cc306d54e55df5e0f60d
Blum Szilárd <szilard.blum@gmail.com>
2023-01-18 12:07:15 +0100

    init

---
+++ src/test/java/flights/FlightTest.java
@@ -0,0 +1,18 @@
+package flights;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightTest {
+
+    @Test
+    void createFlightTest() {
+        Flight flight = new Flight("KLM", "Budapest", "Amsterdam", 132);
+
+        assertEquals("KLM", flight.getAirline());
+        assertEquals("Budapest", flight.getDeparture());
+        assertEquals("Amsterdam", flight.getArrival());
+        assertEquals(132, flight.getNumberOfPassengers());
+    }
+}
